3d2184d295194ab3eb2c9266fcbcc03cb5bb6b9b
Andrew Hollabaugh <ahollaba@robostorm.org>
2017-09-14 19:40:31 -0400

    added basic vision code

--- RRBotAuto.java
+++ RRBotAuto.java
@@ -4,19 +4,23 @@ import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
 import com.qualcomm.robotcore.util.ElapsedTime;
 
+import org.firstinspires.ftc.robotcore.external.navigation.RelicRecoveryVuMark;
+
 /**
  * Created by andrew on 9/10/17.
  */
 
 //Autonomous OpMode for Relic Recovery robot (there will probably be more OpModes)d
 //Currently uses vuforia to determine which pictograph image is displayed
-    
+
 @Autonomous(name = "RRBotAuto")
 public class RRBotAuto extends LinearOpMode
 {
     RRBotHardware robot = new RRBotHardware();
     RRBotVuforiaClass vuforia = new RRBotVuforiaClass();
     private ElapsedTime runtime = new ElapsedTime();
+    enum GlyphPos {LEFT, CENTER, RIGHT}
+    GlyphPos glypPos;
 
     String targetName = null;
 
@@ -28,6 +32,7 @@ public class RRBotAuto extends LinearOpMode
 
         waitForStart();
 
+        //image detection test
         while(opModeIsActive())
         {
             vuforia.Track_Target();
@@ -37,5 +42,14 @@ public class RRBotAuto extends LinearOpMode
                 telemetry.addData("Pictograph", "not visible");
             telemetry.update();
         }
+
+        //red right
+        /*vuforia.Track_Target();
+        while(opModeIsActive() && !vuforia.isVisible)
+        {
+            vuforia.Track_Target();
+        }
+        //if(vuforia.target_name.equals(RelicRecoveryVuMark.LEFT))
+            //glypPos = LEFT;*/
     }
 }
